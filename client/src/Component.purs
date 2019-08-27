b149bf00c3a3946a091bdd7ed2f38700ddbe04ef
seanwestfall <sean.westfall@gmail.com>
2019-08-26 18:35:41 -0700

    updated component.purs

--- client/src/Component.purs
+++ client/src/Component.purs
@@ -3,19 +3,11 @@ module Component where
 import Prelude
 
 import Data.Maybe (Maybe(..))
-import Data.String as String
-import Data.Int (toNumber)
-
-import Effect (Effect)
 import Effect.Aff (Aff)
-import Effect.Console (log, logShow)
+import Effect.Console (log)
 
 import Halogen as H
-import Halogen.Aff as HA
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
-import Halogen.HTML.Elements as HES
-
 import Halogen.Query.EventSource as ES
 import Halogen.HTML.Properties as HP
 
@@ -30,12 +22,15 @@ import Web.UIEvent.KeyboardEvent.EventTypes as KET
 data Action
   = Init
   | HandleKey H.SubscriptionId KeyboardEvent
-  | ConsoleLog
 
-type State = { on :: Boolean
-             , chars :: String
-             , buttonClassName :: String
-             , spritePosition :: Int
+
+-- sprite Position marks the positions of the sprite in aboslute pxs.
+-- sprite style keeps the style attribute properties.
+-- innerStyle keeps the style properties of the div that holds the sprite into place.
+-- spriteHero is the class name of the current sprite frame
+-- spriteReverse keeps track of the direction of the sprite, true means right facing, and false means left facing.
+-- spriteIndex keeps track of the current frame
+type State = { spritePosition :: Int
              , spriteStyle :: String
              , innerStyle :: String
              , spriteHero :: String
@@ -45,10 +40,7 @@ type State = { on :: Boolean
 
 initialState :: State
 initialState =
-  { on: false
-  , chars: ""
-  , buttonClassName: "button-not-changed"
-  , spritePosition: 0
+  { spritePosition: 0
   , spriteStyle: "position: absolute; left: 0;"
   , innerStyle: "display: block; position: relative; margin-left: 20px; margin-right: 20px;"
   , spriteHero: "sprite-hero-0"
@@ -80,6 +72,15 @@ component =
               ]
           []
           ]
+      , HH.div
+          [ HP.class_ (H.ClassName "instructions")
+            , HP.attr (HH.AttrName "style") "display: block; position: fixed; margin-top: 200px;"
+          ]
+          [ HH.p_
+              [ HH.text ("Tap the Left or Right Arrow keys to make the cowboy walk.")]
+            , HH.p_
+              [ HH.text ("Hit the Shfit key plux any other key to make the cowboy hold up his gun.") ]
+          ]
       ]
 
 handleAction :: forall o. Action -> H.HalogenM State Action () o Aff Unit
@@ -92,55 +93,34 @@ handleAction = case _ of
         (HTMLDocument.toEventTarget document)
         (map (HandleKey sid) <<< KE.fromEvent)
   HandleKey sid ev
+    -- The event handler when hitting shift, or when the sprite is holding up his gun
     | KE.shiftKey ev -> do
         H.liftEffect $ E.preventDefault (KE.toEvent ev)
-        _ <- H.modify (\state -> { on: not state.on
-                                   , chars: state.chars
-                                   , buttonClassName: "button-changed"
-                                   , spriteStyle: state.spriteStyle
-                                   , spritePosition: state.spritePosition
-                                   , innerStyle: state.innerStyle
-                                   , spriteIndex: state.spriteIndex
-                                   , spriteReverse: state.spriteReverse
-                                   , spriteHero: "sprite-hero-gun" <> if state.spriteReverse then "r" else ""
-                                 })
+        _ <- H.modify \state -> state 
+                                { spriteHero = "sprite-hero-gun" <> if state.spriteReverse then "r" else ""
+                                }
         H.liftEffect $ log "shiftKey pressed!"
+    -- The event handler for when tapping the left arrow key
     | KE.code ev == "ArrowLeft" -> do
-        _ <- H.modify (\state -> { on: not state.on
-                                   , chars: state.chars
-                                   , buttonClassName: "button-changed"
-                                   , spriteStyle: "position: absolute; left: " <> (show state.spritePosition) <> "px;"
-                                   , spritePosition: state.spritePosition - 10
-                                   , innerStyle: state.innerStyle
-                                   , spriteIndex: if state.spriteIndex /= 3 then state.spriteIndex + 1 else 0
-                                   , spriteReverse: true
-                                   , spriteHero : "sprite-hero-" <> (show state.spriteIndex) <> "r"
-                                 })
+        _ <- H.modify \state -> state 
+                                { spriteStyle = "position: absolute; left: " <> (show state.spritePosition) <> "px;"
+                                  , spritePosition = state.spritePosition - 10
+                                  , spriteIndex = if state.spriteIndex /= 3 then state.spriteIndex + 1 else 0
+                                  , spriteReverse = true
+                                  , spriteHero = "sprite-hero-" <> (show state.spriteIndex) <> "r"
+                                }
         H.liftEffect $ log "Arrow Left Button Clicked!"
+    -- The event handler for when tapping the right arrow key
     | KE.code ev == "ArrowRight" -> do
-        _ <- H.modify (\state -> { on: not state.on
-                                   , chars: state.chars
-                                   , buttonClassName: "button-changed"
-                                   , spriteStyle: "position: absolute; left: " <> (show state.spritePosition) <> "px;"
-                                   , spritePosition: state.spritePosition + 10
-                                   , innerStyle: state.innerStyle
-                                   , spriteIndex: if state.spriteIndex /= 3 then state.spriteIndex + 1 else 0
-                                   , spriteReverse: false
-                                   , spriteHero : "sprite-hero-" <> (show state.spriteIndex)
-                                 })
+        _ <- H.modify \state -> state 
+                                { spriteStyle = "position: absolute; left: " <> (show state.spritePosition) <> "px;"
+                                  , spritePosition = state.spritePosition + 10
+                                  , spriteIndex = if state.spriteIndex /= 3 then state.spriteIndex + 1 else 0
+                                  , spriteReverse = false
+                                  , spriteHero = "sprite-hero-" <> (show state.spriteIndex)
+                                }
         H.liftEffect $ log "Arrow Right Button Clicked!"
     | otherwise ->
         pure unit
-  ConsoleLog -> do
-    _ <- H.modify (\state -> { on: not state.on
-                               , chars: state.chars
-                               , buttonClassName: "button-changed"
-                               , spriteStyle: "position: absolute; left: 0;"
-                               , spritePosition: 0
-                               , innerStyle: state.innerStyle
-                               , spriteIndex: state.spriteIndex
-                               , spriteReverse: state.spriteReverse
-                               , spriteHero : state.spriteHero
-                             })
-    H.liftEffect $ log "Button Clicked!"
+
 
